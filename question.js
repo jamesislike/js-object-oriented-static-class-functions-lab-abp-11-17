9de9dc894c0efa2a824819a439b9c4cd1d7481f4
James Gomis <jamesislike@gmail.com>
2017-11-12 20:54:38 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,21 @@
 'use strict';
+
+class question{
+  constructor(content){
+    this.content = content
+    this.save()
+  }
+
+  save(){
+    this.constructor._All.push(this)         // use  constructor for def instance functions
+  }
+
+  static All(){
+    return this._All;
+  }
+}
+
+Question._All = []
+
+
+new Question("Where in the world is carmen sandiego?")
